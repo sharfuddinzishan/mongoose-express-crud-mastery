eb500f48a8b21de083b9f917b4ebd221e8492f1a
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-11-30 00:21:11 +0600

    User Interface

--- src/app/modules/User/User.interface.ts
+++ src/app/modules/User/User.interface.ts
@@ -1,1 +1,24 @@
-/* eslint-disable no-console */
+export type TOrders = {
+  productName: string
+  price: number
+  quantity: number
+}
+export type TUser = {
+  userId: number
+  fullName: {
+    firstName: string
+    lastName: string
+  }
+  email: string
+  username: string
+  password: string
+  age: number
+  hobbies: string[]
+  address: {
+    street: string
+    city: string
+    country: string
+  }
+  isActive: boolean
+  orders: TOrders
+}
